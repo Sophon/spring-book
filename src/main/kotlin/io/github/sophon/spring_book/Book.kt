1a8df7efe12ce1ebb2ae356fbd1b2edb300bc414
Le Duy Anh <lenduya@gmail.com>
2026-09-17 00:22:04 +0200

    refactor: expand book with ID and rating

--- src/main/kotlin/io/github/sophon/spring_book/Book.kt
+++ src/main/kotlin/io/github/sophon/spring_book/Book.kt
@@ -1,7 +1,9 @@
 package io.github.sophon.spring_book
 
 internal data class Book(
+    val id: Long,
     val title: String,
     val author: String,
     val category: String,
+    val rating: Int? = null,
 )
--- src/main/kotlin/io/github/sophon/spring_book/controller/BookController.kt
+++ src/main/kotlin/io/github/sophon/spring_book/controller/BookController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException
 @RestController
 @RequestMapping("/api/books")
 internal class BookController {
-    private val bookMap: MutableMap<String, Book> = generateBooks()
+    private val bookMap: MutableMap<Long, Book> = generateBooks()
     private val mutex = Mutex()
 
 
@@ -29,11 +29,11 @@ internal class BookController {
         return result
     }
 
-    @GetMapping("/{title}")
-    fun getBook(@PathVariable title: String): Book? {
-        val result = bookMap[title]
+    @GetMapping("/{id}")
+    fun getBook(@PathVariable id: Long): Book? {
+        val result = bookMap[id]
 
-        return result ?: throwNotFound(title)
+        return result ?: throwNotFound(id)
     }
 
     @PostMapping
@@ -41,67 +41,67 @@ internal class BookController {
         @RequestBody book: Book,
     ) {
         mutex.withLock {
-            bookMap.putIfAbsent(book.title, book)
+            bookMap.putIfAbsent(book.id, book)
         }
     }
 
-    @PutMapping("/{title}")
+    @PutMapping("/{id}")
     suspend fun updateBook(
-        @PathVariable title: String,
+        @PathVariable id: Long,
         @RequestBody newBook: Book,
     ) {
         mutex.withLock {
-            if (title !in bookMap) {
-                throwNotFound(title)
+            if (id !in bookMap) {
+                throwNotFound(id)
             } else {
-                bookMap[title] = newBook
+                bookMap[id] = newBook
             }
         }
     }
 
-    @DeleteMapping("/{title}")
+    @DeleteMapping("/{id}")
     suspend fun deleteBook(
-        @PathVariable title: String,
+        @PathVariable id: Long,
     ) {
         mutex.withLock {
-            if (title !in bookMap) {
-                throwNotFound(title)
+            if (id !in bookMap) {
+                throwNotFound(id)
             } else {
-                bookMap.remove(title)
+                bookMap.remove(id)
             }
         }
     }
 
 
-    private fun generateBooks(): MutableMap<String, Book> {
+    private fun generateBooks(): MutableMap<Long, Book> {
         val books = listOf(
-            Book("Pride and Prejudice", "Jane Austen", "Romance"),
-            Book("1984", "George Orwell", "Dystopian"),
-            Book("To Kill a Mockingbird", "Harper Lee", "Fiction"),
-            Book("The Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
-            Book("Moby-Dick", "Herman Melville", "Adventure"),
-            Book("War and Peace", "Leo Tolstoy", "Historical"),
-            Book("Crime and Punishment", "Fyodor Dostoevsky", "Psychological"),
-            Book("The Brothers Karamazov", "Fyodor Dostoevsky", "Philosophical"),
-            Book("Anna Karenina", "Leo Tolstoy", "Romance"),
-            Book("Wuthering Heights", "Emily Brontë", "Gothic"),
-            Book("Jane Eyre", "Charlotte Brontë", "Gothic"),
-            Book("The Odyssey", "Homer", "Epic"),
-            Book("The Iliad", "Homer", "Epic"),
-            Book("Don Quixote", "Miguel de Cervantes", "Adventure"),
-            Book("Les Misérables", "Victor Hugo", "Historical"),
-            Book("The Count of Monte Cristo", "Alexandre Dumas", "Adventure"),
-            Book("Great Expectations", "Charles Dickens", "Fiction"),
-            Book("A Tale of Two Cities", "Charles Dickens", "Historical"),
-            Book("The Adventures of Huckleberry Finn", "Mark Twain", "Adventure"),
-            Book("Frankenstein", "Mary Shelley", "Gothic"),
+            Book(1, "Pride and Prejudice", "Jane Austen", "Romance"),
+            Book(2, "1984", "George Orwell", "Dystopian"),
+            Book(3, "To Kill a Mockingbird", "Harper Lee", "Fiction"),
+            Book(4, "The Great Gatsby", "F. Scott Fitzgerald", "Fiction"),
+            Book(5, "Moby-Dick", "Herman Melville", "Adventure"),
+            Book(6, "War and Peace", "Leo Tolstoy", "Historical"),
+            Book(7, "Crime and Punishment", "Fyodor Dostoevsky", "Psychological"),
+            Book(8, "The Brothers Karamazov", "Fyodor Dostoevsky", "Philosophical"),
+            Book(9, "Anna Karenina", "Leo Tolstoy", "Romance"),
+            Book(10, "Wuthering Heights", "Emily Brontë", "Gothic"),
+            Book(11, "Jane Eyre", "Charlotte Brontë", "Gothic"),
+            Book(12, "The Odyssey", "Homer", "Epic"),
+            Book(13, "The Iliad", "Homer", "Epic"),
+            Book(14, "Don Quixote", "Miguel de Cervantes", "Adventure"),
+            Book(15, "Les Misérables", "Victor Hugo", "Historical"),
+            Book(16, "The Count of Monte Cristo", "Alexandre Dumas", "Adventure"),
+            Book(17, "Great Expectations", "Charles Dickens", "Fiction"),
+            Book(18, "A Tale of Two Cities", "Charles Dickens", "Historical"),
+            Book(19, "The Adventures of Huckleberry Finn", "Mark Twain", "Adventure"),
+            Book(20, "Frankenstein", "Mary Shelley", "Gothic"),
         )
-            .associateBy { it.title }
+            .associateBy { it.id }
             .toMutableMap()
         return books
     }
 
-    private fun throwNotFound(title: String): Nothing {
-        throw ResponseStatusException(HttpStatus.NOT_FOUND, "Book $title not found.")
+    private fun throwNotFound(id: Long): Nothing {
+        throw ResponseStatusException(HttpStatus.NOT_FOUND, "Book $id not found.")
     }
 }
